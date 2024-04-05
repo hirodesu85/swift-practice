0e8e2ef10e1e8948064f4d7a27fee7bdd7f3b3ad
OGATA Hiromu <hiromukkun@gmail.com>
2024-04-06 01:03:04 +0900

    add:プロジェクト追加2

---
+++ SwiftPractice/SwiftPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPractice
+//
+//  Created by 尾形拓夢 on 2024/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPractice/SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by 尾形拓夢 on 2024/04/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
