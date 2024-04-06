1f2c4a116a650a4119f3e038d14c25a7bb3e9958
OGATA Hiromu <hiromukkun@gmail.com>
2024-04-06 11:57:42 +0900

    add:part3のベースコード追加

---
+++ HelloStack/HelloStack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloStack
+//
+//  Created by 尾形拓夢 on 2024/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloStack/HelloStack/HelloStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloStackApp.swift
+//  HelloStack
+//
+//  Created by 尾形拓夢 on 2024/04/06.
+//
+
+import SwiftUI
+
+@main
+struct HelloStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
