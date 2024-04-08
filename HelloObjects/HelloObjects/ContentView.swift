2036ffcdfae93e45ebdaef9a5930d4b374099be5
OGATA Hiromu <hiromukkun@gmail.com>
2024-04-08 23:18:34 +0900

    add:part3の学習開始

---
+++ HelloObjects/HelloObjects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloObjects
+//
+//  Created by 尾形拓夢 on 2024/04/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+            Button("ボタン") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloObjects/HelloObjects/HelloObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloObjectsApp.swift
+//  HelloObjects
+//
+//  Created by 尾形拓夢 on 2024/04/07.
+//
+
+import SwiftUI
+
+@main
+struct HelloObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
