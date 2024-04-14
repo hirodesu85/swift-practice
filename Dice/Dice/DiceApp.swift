b1bfe3f95598de069bd669485a702fe2e91506f0
OGATA Hiromu <hiromukkun@gmail.com>
2024-04-14 14:34:18 +0900

    add:part6の内容追加

---
+++ Dice/Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by 尾形拓夢 on 2024/04/14.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
